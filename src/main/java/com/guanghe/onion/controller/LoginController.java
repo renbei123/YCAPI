15e27dd3fcf898155c6008bf46c2ed7059253d4e
renjie <renjie@guanghe.tv>
2018-12-28 20:49:24 +0800

    first

---
+++ src/main/java/com/guanghe/onion/controller/LoginController.java
@@ -0,0 +1,56 @@
+package com.guanghe.onion.controller;
+
+
+import com.guanghe.onion.dao.UserJPA;
+import com.guanghe.onion.entity.UserEntity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.domain.Specification;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
+import javax.servlet.http.HttpServletRequest;
+import java.util.Optional;
+
+
+@RestController
+@RequestMapping(value = "/user")
+public class LoginController {
+
+    @Autowired
+    private UserJPA userJPA;
+
+    @RequestMapping(value = "/login")
+    public String login(UserEntity user, HttpServletRequest request)
+    {
+        //登录成功
+        boolean flag = true;
+        String result = "登录成功";
+        //根据用户名查询用户是否存在
+        Optional<UserEntity> userEntity = userJPA.findOne(new Specification<UserEntity>() {
+            @Override
+            public Predicate toPredicate(Root<UserEntity> root, CriteriaQuery<?> criteriaQuery, CriteriaBuilder criteriaBuilder) {
+                criteriaQuery.where(criteriaBuilder.equal(root.get("name"), user.getName()));
+                return null;
+            }
+        });
+        //用户不存在
+        if(userEntity == null){
+            flag = false;
+            result = "用户不存在，登录失败";}
+        //密码错误
+        else if(!userEntity.get().getPwd().equals(user.getPwd())){
+            flag = false;
+            result = "用户密码不相符，登录失败";
+        }
+        //登录成功
+        if(flag){
+            //将用户写入session
+            request.getSession().setAttribute("_session_user",userEntity);
+        }
+        return result;
+    }
+}
