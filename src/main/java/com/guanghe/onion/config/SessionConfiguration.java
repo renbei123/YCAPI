1ef91f863aacf12a8a2f641a69d416f0c8c531ea
renjie <renjie@guanghe.tv>
2019-02-22 19:03:23 +0800

    update

---
+++ src/main/java/com/guanghe/onion/config/SessionConfiguration.java
@@ -0,0 +1,25 @@
+package com.guanghe.onion.config;
+
+
+import com.guanghe.onion.interceptor.SessionInterceptor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * ========================
+ * Created with IntelliJ IDEA.
+ * User：恒宇少年
+ * Date：2017/4/7
+ * Time：21:47
+ * 码云：http://git.oschina.net/jnyqy
+ * ========================
+ */
+@Configuration
+public class SessionConfiguration extends WebMvcConfigurerAdapter
+{
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new SessionInterceptor()).addPathPatterns("/aa");
+    }
+}
