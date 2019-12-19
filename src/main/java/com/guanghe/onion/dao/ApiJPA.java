dedc84ac7ad972d33c295e94eb96b9132c0e33ca
renjie <renjie@guanghe.tv>
2019-12-19 12:04:06 +0800

    update

--- src/main/java/com/guanghe/onion/dao/ApiJPA.java
+++ src/main/java/com/guanghe/onion/dao/ApiJPA.java
@@ -1,12 +1,9 @@
 package com.guanghe.onion.dao;
 
 import com.guanghe.onion.entity.Api;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
-import java.io.Serializable;
 import java.util.List;
 
 public interface ApiJPA extends BaseJPA<Api> {
@@ -14,6 +11,11 @@ public interface ApiJPA extends BaseJPA<Api> {
     @Query(value = "SELECT id, name, path,method,headers,body,remarks from api "
             ,  nativeQuery = true)
     List<Object[]> selectmyapi();
+
+
+    @Query(value = "SELECT  path from api where creater=:creater", nativeQuery = true)
+    List<String> findPathByCreater(@Param("creater") String creater);
+
 
 
 }
